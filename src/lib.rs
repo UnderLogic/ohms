58d4595bf1814218d0fc4b814a99fb67aba6c172
Erik Rogers <erik@underlogic.tech>
2023-02-03 04:09:01 -0500

    fix: docs and prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Electrical unit types for embedded targets focusing on ease-of-use and performance
+//!
+//! Supported units:
+//! - Current (μA, mA, A)
+//! - Resistance (mΩ, Ω, kΩ, MΩ)
+//! - Voltage (μV, mV, V)
+//!
+//! Each unit type has a corresponding extension trait for creating values from `i32`/`u32` and `f32`
+//! values. The extension traits are named `ExtI32`, `ExtU32`, and `ExtF32` respectively.
+//!
+//! Unit types can easily be converted to and from different denominations.
+//!
 #![no_std]
 
 mod assert;
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,3 @@
-pub use crate::current::*;
-pub use crate::resistance::*;
-pub use crate::voltage::*;
+pub use crate::current::{Current, ExtF32 as CurrentExtF32, ExtU32 as CurrentExtU32};
+pub use crate::resistance::{ExtF32 as ResistanceExtF32, ExtU32 as ResistanceExtU32, Resistance};
+pub use crate::voltage::{ExtF32 as VoltageExtF32, ExtI32 as VoltageExtI32, Voltage};
